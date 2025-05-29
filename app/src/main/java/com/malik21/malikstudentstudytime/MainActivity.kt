87ba0c5d66463d3daccb72dd2aaf8042f39b8e09
garvit_malik <garvit.malik@my.jcu.edu.au>
2025-05-29 17:00:21 +1000

    column layout

--- app/src/main/java/com/malik21/malikstudentstudytime/MainActivity.kt
+++ app/src/main/java/com/malik21/malikstudentstudytime/MainActivity.kt
@@ -1,26 +1,39 @@
 package com.malik21.malikstudentstudytime
 
+import android.R
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.malik21.malikstudentstudytime.ui.theme.MalikStudentStudyTimeTheme
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.HorizontalAlignmentLine
+import androidx.compose.ui.text.style.LineHeightStyle
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        //enableEdgeToEdge()
         setContent {
             MalikStudentStudyTimeTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
+                    TestLayout(
                         name = "Android",
                         modifier = Modifier.padding(innerPadding)
                     )
@@ -31,17 +44,29 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun TestLayout(name: String, modifier: Modifier = Modifier) {
+    Row(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Column(modifier = Modifier.fillMaxHeight().width(100.dp).background(Color.Yellow)) {
+            Text(text = "Column 1")
+        }
+        Column(modifier = Modifier.fillMaxHeight().width(100.dp).background(Color.Gray)) {
+            Text(text = "Column 2")
+        }
+        Column(modifier = Modifier.fillMaxHeight().fillMaxWidth().background(Color.Green),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally){
+            Text(text = "Column 3")
+        }
+
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     MalikStudentStudyTimeTheme {
-        Greeting("Android")
+        TestLayout("Android")
     }
 }
