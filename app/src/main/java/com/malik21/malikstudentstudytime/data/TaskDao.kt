398c3efea0a83fc88d75bd7c29f56f7086e9c046
garvit_malik <garvit.malik@my.jcu.edu.au>
2025-07-16 18:01:05 +1000

    add TaskDao

--- app/src/main/java/com/malik21/malikstudentstudytime/data/TaskDao.kt
+++ app/src/main/java/com/malik21/malikstudentstudytime/data/TaskDao.kt
@@ -1,2 +1,19 @@
 package com.malik21.malikstudentstudytime.data
 
+import androidx.room.*
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface TaskDao {
+    @Query("SELECT * FROM tasks ORDER BY id DESC")
+    fun getAllTasks(): Flow<List<TaskItem>>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertTask(task: TaskItem)
+
+    @Update
+    suspend fun updateTask(task: TaskItem)
+
+    @Delete
+    suspend fun deleteTask(task: TaskItem)
+}
