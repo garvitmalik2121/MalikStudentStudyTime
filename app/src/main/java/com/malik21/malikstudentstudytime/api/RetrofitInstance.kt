5f38055a49c679c8e392fbc5a8ba13d18fdb4d3a
garvit_malik <garvit.malik@my.jcu.edu.au>
2025-07-16 18:04:19 +1000

    add RetrofitInstance

--- app/src/main/java/com/malik21/malikstudentstudytime/api/RetrofitInstance.kt
+++ app/src/main/java/com/malik21/malikstudentstudytime/api/RetrofitInstance.kt
@@ -1,2 +1,16 @@
 package com.malik21.malikstudentstudytime.api
 
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object RetrofitInstance {
+    private const val BASE_URL = "https://api.quotable.io/"
+
+    val api: QuoteApi by lazy {
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(QuoteApi::class.java)
+    }
+}
