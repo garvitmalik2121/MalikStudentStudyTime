0939ac6191eda82ba0fe5fdb733fee86a0029551
garvit_malik <garvit.malik@my.jcu.edu.au>
2025-07-16 18:00:31 +1000

    add TaskEntity

--- app/src/main/java/com/malik21/malikstudentstudytime/data/TaskEntity.kt
+++ app/src/main/java/com/malik21/malikstudentstudytime/data/TaskEntity.kt
@@ -1,2 +1,12 @@
 package com.malik21.malikstudentstudytime.data
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "tasks")
+data class TaskItem(
+    val title: String,
+    val dueDate: String? = null,
+    val isCompleted: Boolean = false,
+    @PrimaryKey(autoGenerate = true) val id: Int = 0
+)
