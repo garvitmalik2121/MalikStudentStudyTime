869ba7268dd1a73bcfcbb39068ddb3adf7b91b4e
garvit_malik <garvit.malik@my.jcu.edu.au>
2025-07-16 17:50:58 +1000

    Update navigation

--- app/src/main/java/com/malik21/malikstudentstudytime/AppNavigation.kt
+++ app/src/main/java/com/malik21/malikstudentstudytime/AppNavigation.kt
@@ -1,2 +1,17 @@
 package com.malik21.malikstudentstudytime
 
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import com.malik21.malikstudentstudytime.screen.*
+
+@Composable
+fun AppNavigation(navController: NavHostController) {
+    NavHost(navController, startDestination = "dashboard") {
+        composable("dashboard") { DashboardScreen(navController) }
+        composable("tasks") { TasksScreen(navController) }
+        composable("account") { AccountScreen(navController) }
+        composable("settings") { SettingsScreen(navController) }
+    }
+}
