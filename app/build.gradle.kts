eaea14126e70ee004591ba8c1886216c30f13e54
garvit_malik <garvit.malik@my.jcu.edu.au>
2025-07-17 20:30:05 +1000

    add dependencies for testing

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -94,4 +94,8 @@ dependencies {
     implementation("androidx.datastore:datastore-preferences:1.0.0")
     implementation("androidx.compose.material:material-icons-extended:<compose_version>")
 
+    // Instrumented testing dependencies
+    androidTestImplementation ("androidx.test.ext:junit:1.1.5")      // JUnit for Android tests
+    androidTestImplementation ("androidx.test.espresso:espresso-core:3.5.1") // Espresso UI testing
+
 }
